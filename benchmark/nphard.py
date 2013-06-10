3b15ff608915282d3e996f96046cc14807c48132
Juan Lopes <me@juanlopes.net>
2013-06-09 23:23:50 -0300

    conclusão

--- benchmark/nphard.py
+++ benchmark/nphard.py
@@ -2,6 +2,8 @@
 import re
 
 clauses = [[1,2,-3], [1,-2,3], [-1,-2,3], [-1,-2,-3]]
+#(x1 or not x2 or x3) and (x1 or not x2 or x3) and 
+#(not x1 or not x2 or x3) and (not x1 or not x2 or not x3)
 
 v = reduce(lambda a, b: max(a, *b), clauses, 0)
 s = v * 'x' + ';' + len(clauses) * 'x,'
